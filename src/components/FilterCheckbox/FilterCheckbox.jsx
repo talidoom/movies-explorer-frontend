97a4e8b4ba233286f7ca1db8bbb60278743bb4bb
talidoom <talidoom@yandex.ru>
2024-01-14 15:14:33 +0300

    test

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -1,10 +1,39 @@
 import './FilterCheckbox.css';
+import { useLocation } from 'react-router-dom';
+import React from 'react';
 
-const FilterCheckbox = () => {
+const FilterCheckbox = ({ setIsShortMovie, isShortMovie }) => {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    if (pathname === "/movies") {
+      setIsShortMovie(JSON.parse(localStorage.getItem("checkboxState")) || false);
+    }
+    else {
+      setIsShortMovie(
+        JSON.parse(localStorage.getItem("savedCheckboxState")) || false
+      );
+    }
+  }, [pathname, setIsShortMovie]);
+
+  const handleCheckbox = () => {
+    setIsShortMovie(!isShortMovie);
+    if (pathname === "/saved-movies") {
+      localStorage.setItem("savedCheckboxState", !isShortMovie);
+    } else {
+      localStorage.setItem("checkboxState", !isShortMovie);
+    }
+  };
+
   return (
     <section className='filter'>
       <label className='filter__switch'>
-        <input className='filter__check' type='checkbox' />
+        <input
+          className='filter__check'
+          checked={isShortMovie}
+          onChange={handleCheckbox}
+          type='checkbox'
+        />
         <span className='filter__container'></span>
       </label>
       <p className='filter__title'>Короткометражки</p>
