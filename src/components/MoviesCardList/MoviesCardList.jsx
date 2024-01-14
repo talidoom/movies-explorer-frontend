c5dacab04f82f185a8e63f3fcea1425a3e7a1a51
talidoom <talidoom@yandex.ru>
2024-01-14 15:28:01 +0300

    test

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -13,10 +13,27 @@ const MoviesCardList = ({
   setSaveMovies,
   isShortMovie,
   mobile,
-  tablet }) => {
+  tablet,
+  searchSavedMovies }) => {
 
   const location = useLocation();
   const currentUser = useContext(CurrentContext);
+
+  const filterMovie = isShortMovie ? trueMovies.filter((movie) => movie.duration <= 40) : trueMovies;
+  const saveCurrentUser = saveMovies.filter(
+    (movie) => movie.owner === currentUser._id
+  );
+  const filterSavedMovie = isShortMovie ? saveCurrentUser.filter((movie) => movie.duration <= 40) : saveCurrentUser;
+  const filterSavedMovieSearch = searchSavedMovies !== ""
+      ? filterSavedMovie.filter((movie) =>
+          movie.nameRU.toLowerCase().includes(searchSavedMovies?.toLowerCase())
+        ) : filterSavedMovie;
+
+  function getSavedMovie(arr, movie) {
+    return arr.find((item) => {
+      return item.movieId === movie.id;
+    });
+  }
 
   return (
     <>
